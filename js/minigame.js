6d764340881023b3069d5474e736ffdb8e8045e2
Max-Lem3 <141814660+Max-Lem3@users.noreply.github.com>
2023-08-09 15:42:22 +0200

    Server Reihen

--- js/minigame.js
+++ js/minigame.js
@@ -1,21 +1,49 @@
+
 function reward() {
   alert("Du bekommst eine Belohnung");
 }
 
-function blinkeZufällig() {
-  let wartezeit = Math.floor(Math.random() * 10);
-  setTimeout(wartezeit);
+function blink_blue() {
+  $(".blink").css("background-color", "blue");
 }
-
-function blink() {
-  setInterval(function () {
-    element.innerHTML += "Hello";
-  }, 1000);
+function blink_red() {
+  $(".blink").css("background-color", "red");
 }
 
 $(document).ready(function () {
-  $(".blink").click(function () {
-    $(".blink").blink();
-    alert("blinkt");
-  });
+  for (let i = 0; i < 40; i++) {
+    //Timer without function
+    setInterval(changeColor(i), 3000);
+  }
+});
+
+function changeColor(i) {
+  if (i % 2 == 0) blink_blue();
+  else blink_red();
+}
+
+function generateRandom(max) {
+    let x = Math.floor((Math.random() * max) + 1);
+    return x;
+}
+
+$(document).ready(function () {
+  let zufall;
+  zufall = generateRandom(3);
+  if (zufall == 1) {
+    console.log("zufall==1");
+    zufall = generateRandom(5);
+    if (zufall == 0) {
+        
+    }
+    if (zufall == 1) {
+
+    }
+    if (zufall == 2) {
+
+    }
+    if (zufall == 3) {
+
+    }
+  }
 });
