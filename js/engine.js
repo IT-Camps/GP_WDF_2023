db32319edd116b97d53d2c2db95f79170d1b62e4
Jonah-12 <jonah@neckar-sume.de>
2023-08-09 16:38:46 +0200

    spielfeld leeren

--- js/engine.js
+++ js/engine.js
@@ -14,12 +14,7 @@ function ladeBlocksInArray(levelName) {
 
     // Alternative:
 
-    // for(let y = 1; y <= height; y++) {
-	// 	spielfeld[y] = [];
-	//  	for(let x = 1; x <= width; x++) {
-	// 		spielfeld[y][x] = erzeugeFeld(y, x, 'floor', false, false);
-	// 	}
-	// }
+    // 
 
     return levelblocks;
 }
@@ -32,6 +27,26 @@ function zeigeSpielfeld(spielfeld) {
 			$('#spielfeld').append('<div class="' + spielfeld[x][y].material +  '" id="' + spielfeld[x][y].x + '/' + spielfeld[x][y].y + '">');
 		}
 }}
+
+function blockAuswechseln(spielfeld, x, y, material, solid, interactive) {
+    delete spielfeld[y][x];
+    console.log(spielfeld);
+    spielfeld[y][x] = {x: x, y: y, material: material, solid: solid, interactive: interactive}
+
+    return spielfeld
+}
+
+function spielfeldLeeren() {
+    let current;
+    for(let y = 0; y < HEIGHT; y++) {
+     	spielfeld[y] = [];
+     	for(let x = 0; x < WIDTH; x++) {
+            current = document.getElementById(x + '/' + y);
+            current.remove();
+     	}
+    }
+}
+
 function starteEngine() {
     console.log("Starte engine...");
     console.log(`Geladene level: ${LEVEL.map(l => l.name).join(', ')}`);
@@ -39,9 +54,8 @@ function starteEngine() {
     let levelblocks = ladeBlocksInArray("demoLevel");
     console.log(levelblocks);
     zeigeSpielfeld(levelblocks);
-    /*
-    delete levelblocks[0][0];
-    console.log(levelblocks);
-    levelblocks[0][0] = {x: 0, y: 0, material: 'floor', solid: false, interactive: false}
-    */
+    spielfeldLeeren();
+    levelblocks = blockAuswechseln(levelblocks, 0, 0, 'server', true, true);
+    zeigeSpielfeld(levelblocks);
+
 }
