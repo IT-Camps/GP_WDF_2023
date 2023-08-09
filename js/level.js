0acdc99d941b23dabf7b47f39b1fae9dcb992890
Christoph-Geyer-10 <64601197+sebastian-rt@users.noreply.github.com>
2023-08-09 12:18:26 +0200

    Demo level update

--- js/level.js
+++ js/level.js
@@ -2,15 +2,14 @@ let LEVEL = [
     {
         name: "demoLevel",
         data: [
-            { x: 1, y: 1, material: 'grass', solid: true },
-            { x: 2, y: 1, material: 'grass', solid: true },
-            { x: 3, y: 1, material: 'grass', solid: true },
-            { x: 4, y: 2, material: 'grass', solid: true },
-            { x: 5, y: 2, material: 'grass', solid: true },
-            { x: 6, y: 3, material: 'grass', solid: true },
-            { x: 7, y: 2, material: 'grass', solid: true },
-            { x: 8, y: 2, material: 'grass', solid: true },
-            { x: 9, y: 1, material: 'grass', solid: true },
+            { x: 2, y: 3, material: 'grass', solid: true },
+            { x: 3, y: 4, material: 'grass', solid: true },
+            { x: 4, y: 4, material: 'grass', solid: true },
+            { x: 5, y: 4, material: 'grass', solid: true },
+            { x: 6, y: 5, material: 'grass', solid: true },
+            { x: 7, y: 4, material: 'grass', solid: true },
+            { x: 8, y: 4, material: 'grass', solid: true },
+            { x: 9, y: 3, material: 'grass', solid: true },
 
         ]
     }
