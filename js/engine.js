ed232618ea09393f6745bcaad3ae9c34733a9da0
Jonah-12 <jonah@neckar-sume.de>
2023-08-09 15:00:19 +0200

    Level blocks in array laden

--- js/engine.js
+++ js/engine.js
@@ -1,10 +1,25 @@
-function zeichneSpielbrett(levelName) {
-    //  Spielbrett team implentiert logik hier
+let levelblocks = [ ];
+function ladeBlocksInArray(levelName) {
+
+    const level = LEVEL.find(l => l.name == levelName);
+    for (let x = 0; x < 20; x++) {
+        levelblocks[x]= [];
+        for (let y = 0; y < 15; y++) {
+            levelblocks[x][y] = level.data.find(block => block.x == x && block.y == y) || {x: x, y: y, material: 'floor', solid: false};
+        }
+    }
+    console.log(levelblocks);
+}
+
+
+
+function zeichneSpielbrett() {
+
 }
 
 function starteEngine() {
     console.log("Starte engine...");
     console.log(`Geladene level: ${LEVEL.map(l => l.name).join(', ')}`);
 
-    zeichneSpielbrett("demoLevel");
+    ladeBlocksInArray("demoLevel");
 }
