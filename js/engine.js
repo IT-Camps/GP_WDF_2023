e1c8e22c47018848c49f0fdcbfe2ea1adfba0f49
sebastian-rt <64601197+sebastian-rt@users.noreply.github.com>
2023-08-10 09:27:21 +0200

    unnötige levelblocks zuweisung entfernt
    levelblocks Variable entfernt, da ladeBlocksInArray nichts zurück gibt

--- js/engine.js
+++ js/engine.js
@@ -6,48 +6,47 @@ const HOEHE = 15;
 function ladeBlocksInArray(levelName) {
     const level = LEVEL.find(l => l.name == levelName);
     for (let x = 0; x < HOEHE; x++) {
-        spielfeld[x]= [];
+        spielfeld[x] = [];
         for (let y = 0; y < BREITE; y++) {
-            spielfeld[x][y] = level.data.find(block => block.x == x && block.y == y) || {x: x, y: y, material: 'floor', solid: false, interactive: false};
+            spielfeld[x][y] = level.data.find(block => block.x == x && block.y == y) || { x: x, y: y, material: 'floor', solid: false, interactive: false };
         }
     }
 
-
 }
 
-
 function zeigeSpielfeld() {
-	for(let x = 0; x < HOEHE; x++) {
-		for(let y = 0; y < BREITE; y++) {
-			$('#spielfeld').append('<div class="' + spielfeld[x][y].material +  '" id="' + x + '/' + y + '"></div>');
-		}
-}}
+    for (let x = 0; x < HOEHE; x++) {
+        for (let y = 0; y < BREITE; y++) {
+            $('#spielfeld').append('<div class="' + spielfeld[x][y].material + '" id="' + x + '/' + y + '"></div>');
+        }
+    }
+}
 
 function blockAuswechseln(spielfeld, x, y, material, solid, interactive) {
     delete spielfeld[y][x];
     console.log(spielfeld);
-    spielfeld[y][x] = {x: x, y: y, material: material, solid: solid, interactive: interactive}
+    spielfeld[y][x] = { x: x, y: y, material: material, solid: solid, interactive: interactive }
 
     return spielfeld
 }
 
 function spielfeldLeeren() {
     let current;
-    for(let y = 0; y < HOEHE; y++) {
-     	spielfeld[y] = [];
-     	for(let x = 0; x < BREITE; x++) {
+    for (let y = 0; y < HOEHE; y++) {
+        spielfeld[y] = [];
+        for (let x = 0; x < BREITE; x++) {
             current = document.getElementById(x + '/' + y);
             current.remove();
-     	}
+        }
     }
 }
 
 function starteEngine() {
-    let level = LEVEL.map(l => l.name).join(', ');
+    let levelNamen = LEVEL.map(l => l.name);
     console.log("Starte engine...");
-    console.log(`Geladene level: `+level);
+    console.log(`Geladene level: ` + levelNamen.join(', '));
 
-    let levelblocks = ladeBlocksInArray("serverraum");
+    ladeBlocksInArray("serverraum");
     zeigeSpielfeld();
     /*
     spielfeldLeeren();
@@ -57,7 +56,7 @@ function starteEngine() {
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     starteEngine();
     console.log(spielfeld);
 });
