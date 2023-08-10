e43d1707db05aaf11c7c1608dc776ae340bab33a
Max-Lem3 <141814660+Max-Lem3@users.noreply.github.com>
2023-08-10 16:01:49 +0200

    FunktioniertbisaufScore

--- js/minigame.js
+++ js/minigame.js
@@ -1,3 +1,183 @@
+$(document).ready(function () {
+  let server1 = true;
+  let server2 = true;
+  let server3 = true;
+  let server4 = true;
+  let server5 = true;
+  let server6 = true;
+  let server7 = true;
+  let server8 = true;
+  let server9 = true;
+  let server10 = true;
+  let server11 = true;
+  let server12 = true;
+  const meinScore = document.getElementById("score");
+  const meinDiv = document.getElementById("timer");
+  var score = 0;
+  var timeleft = 15;
+  meinDiv.innerHTML = `<p> Zeit:${timeleft}</p>`;
+  meinScore.innerHTML = `<p>Score:${score}</p>`;
+  if (
+    $("#button").on("click", (e) => {
+      if (timeleft >= 15 || timeleft <= 1) {
+        alleServerNeu();
+        timeleft = 15;
+        var downloadTimer = setInterval(function () {
+          timeleft = timeleft - 0.5;
+          if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            console.log(timeleft);
+          }
+          meinDiv.innerHTML = `<p>Zeit:${timeleft}</p>`;
+          serverRandomZerstören();
+        }, 500);
+      }
+    })
+  );
+
+  //Generiere Server Reihen und Spalten
+  for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 4; j++) {
+      $("#server > .server-reihe" + i + " >.server" + j + "").on(
+        "click",
+        (e) => {
+          console.log("reihe");
+          let zahl = 1;
+          if (zahl == 2) {
+            console.log("image");
+            $("#server" + j + "").css(
+              "background-image",
+              "url(../img/spielFeld/SSF.png)"
+            );
+            zahl--;
+            console.log(zahl + "von zahl2");
+          }
+          if (zahl == 1) {
+            console.log("image2");
+            $("#server" + j).css(
+              "background-image",
+              "url(../img/spielFeld/SSF.png)"
+            );
+            zahl++;
+            console.log(zahl + "von zahl1");
+          }
+        }
+      );
+    }
+  }
+  $("#server1").click(function () {
+    if (timeleft != 0) {
+      console.log(server1);
+      if (server1) {
+        console.log("server 1 wurde angeklickt");
+        $("#server1").css("background-image", "url(../img/spielFeld/SSF.png)");
+        console.log("Bild richtig" + server1);
+        score++;
+        server1 = true;
+      }
+    }
+  });
+  $("#server2").click(function () {
+    if (timeleft != 0) {
+      if (server2) {
+        $("#server2").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server2 = true;
+      }
+    }
+  });
+  $("#server3").click(function () {
+    if (timeleft != 0) {
+      if (server3) {
+        $("#server3").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server3 = true;
+      }
+    }
+  });
+  $("#server4").click(function () {
+    if (timeleft != 0) {
+      if (server4) {
+        $("#server4").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server4 = true;
+      }
+    }
+  });
+  $("#server5").click(function () {
+    if (timeleft != 0) {
+      if (server5 ) {
+        $("#server5").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server5 = true;
+      }
+    }
+  });
+  $("#server6").click(function () {
+    if (timeleft != 0) {
+      if (server6 ) {
+        $("#server6").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server6 = true;
+      }
+    }
+  });
+  $("#server7").click(function () {
+    if (timeleft != 0) {
+      if (server7 ) {
+        $("#server7").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server7 = true;
+      }
+    }
+  });
+  $("#server8").click(function () {
+    if (timeleft != 0) {
+      if (server8 ) {
+        $("#server8").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server8 = true;
+      }
+    }
+  });
+  $("#server9").click(function () {
+    if (timeleft != 0) {
+      if (server9) {
+        $("#server9").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server9 = true;
+      }
+    }
+  });
+  $("#server10").click(function () {
+    if (timeleft != 0) {
+      if (server10) {
+        $("#server10").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server10 = true;
+      }
+    }
+  });
+  $("#server11").click(function () {
+    if (timeleft != 0) {
+      if (server11) {
+        $("#server11").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server11 = true;
+      }
+    }
+  });
+  $("#server12").click(function () {
+    if (timeleft != 0) {
+      if (server12 ) {
+        $("#server12").css("background-image", "url(../img/spielFeld/SSF.png)");
+        score++;
+        server12 = true;
+      }
+    }
+  });
+});
+
 function reward() {
   alert("Du bekommst eine Belohnung");
 }
@@ -8,13 +188,6 @@ function blink_blue() {
 function blink_red() {
   $(".blink").css("background-color", "red");
 }
-
-$(document).ready(function () {
-  for (let i = 0; i < 40; i++) {
-    //Timer without function
-    setInterval(changeColor(i), 3000);
-  }
-});
 
 function changeColor(i) {
   if (i % 2 == 0) blink_blue();
@@ -31,103 +204,117 @@ function serverRandomZerstören() {
     case 0:
       switch (generateRandom(4)) {
         case 0:
-          $(".server-reihe1>#server1").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe1>#server1").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          console.log("Server1 auf false versuc");
+          server1 = false;
+          console.log("Server1 auf false?" + server1);
           break;
         case 1:
-          $(".server-reihe1>#server2").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe1>#server2").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server2 = false;
           break;
         case 2:
-          $(".server-reihe1>#server3").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe1>#server3").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server3 = false;
           break;
         case 3:
-          $(".server-reihe1>#server4").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe1>#server4").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server4 = false;
           break;
         default:
           break;
       }
-     break;
+      break;
     case 1:
       switch (generateRandom(4)) {
         case 0:
-          $(".server-reihe2>#server5").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe2>#server5").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server5 = false;
           break;
         case 1:
-          $(".server-reihe2>#server6").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe2>#server6").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server6 = false;
           break;
         case 2:
-          $(".server-reihe2>#server7").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe2>#server7").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server7 = false;
           break;
         case 3:
-          $(".server-reihe2>#server8").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe2>#server8").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server8 = false;
           break;
         default:
           break;
       }
-     break;
+      break;
     case 2:
       switch (generateRandom(4)) {
         case 0:
-          $(".server-reihe3>#server9").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe3>#server9").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server9 = false;
           break;
         case 1:
-          $(".server-reihe3>#server10").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe3>#server10").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server10 = false;
           break;
         case 2:
-          $(".server-reihe3>#server11").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe3>#server11").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server11 = false;
           break;
         case 3:
-          $(".server-reihe3>#server12").css("background-image", "url(../img/spielFeld/SRB.png)");
+          $(".server-reihe3>#server12").css(
+            "background-image",
+            "url(../img/spielFeld/SRF.png)"
+          );
+          server12 = false;
           break;
         default:
           break;
       }
-     break;
+      break;
     default:
       break;
-      
   }
+}
 
-};
-
-$(document).ready(function () {
-  if ($("#button").on("click", e => {
-    var timeleft = 15;
-    var downloadTimer = setInterval(function(){
-      timeleft--;
-      if(timeleft <= 0){
-          clearInterval(downloadTimer);
-          console.log(timeleft);
-          
-      }
-    serverRandomZerstören();
-    },1000);
-    })
+function alleServerNeu() {
+  for (let i = 1; i <= 12; i++) {
+    $("#server" + i + "").css(
+      "background-image",
+      "url(../img/spielFeld/SSF.png)"
     );
-  
-
-
-  //Generiere Server Reihen und Spalten
-  for (let i = 1; i <= 3; i++) {
-    for (let j = 1; j <= 4; j++) {
-      console.log("for");
-      $("#server > .server-reihe" + i + " >.server" + j + "").on("click", (e) => {
-        console.log("reihe");
-        let zahl = 1;
-        if (zahl==2){
-          console.log("image");
-          $("#server"+j+"").css("background-image", "url(url(../img/spielFeld/SSD.png))");
-          zahl--;
-          console.log(zahl +"von zahl2");
-        }
-        if (zahl ==1){
-          console.log("image2");
-          $("#server" + j).css("background-image", "url(../img/spielFeld/SSD.png)");
-          zahl++;
-          console.log(zahl +"von zahl1");
-        }
-      }); 
-    }}
-
-});
-
-
+  }
+}
