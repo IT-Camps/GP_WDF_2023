8a7e81f3eb0818ae496a014e41fd8959435e92d5
Hyperx3D <hyperx3d@gmail.com>
2023-08-11 10:17:13 +0200

    zgfi

--- js/shadow.js
+++ js/shadow.js
@@ -1,6 +1,3 @@
 //<div>
 //$(document).getElementById("myDIV").style.boxShadow = "10px 20px 30px blue";
 //</div>
-function myFunction() {
-    document.getElementById("#spielfigur").style.boxShadow = "30px 30px 30px 30px black"; //verbunden mit Spielfeld.css 
-}
