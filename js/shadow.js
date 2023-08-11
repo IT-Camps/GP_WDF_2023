16a998b64d54e1cd74e713fd084adbe97c9c9b53
Hyperx3D <hyperx3d@gmail.com>
2023-08-11 11:22:19 +0200

    sichtbeschränkung bearbeitet

--- js/shadow.js
+++ js/shadow.js
@@ -1,3 +1,14 @@
 //<div>
 //$(document).getElementById("myDIV").style.boxShadow = "10px 20px 30px blue";
 //</div>
+/*function myFunction() {
+    document.getElementById("#spielfigur").style.boxShadow = "30px 30px 30px 30px black"; //verbunden mit Spielfeld.css 
+}*/
+
+function shadowFolgtFigur() {
+    $("sichteinschraenkung.png").css("top",+1125 );
+    $("sichteinschraenkung.png").css("left",+875);
+}
+$(document).on('keydown', (function(){//hier wird auf keydown bezogern weil beoim bewegen der figur eine taste geklickt wird
+    mausFolgtZeiger()
+}));
