9119c34af3db4a4ebef075db000beb3ed2280086
Hyperx3D <hyperx3d@gmail.com>
2023-08-11 09:32:45 +0200

    create shadow als funktion in verbindung mit spielfeld.css

---
+++ js/shadow.js
@@ -0,0 +1,6 @@
+//<div>
+//$(document).getElementById("myDIV").style.boxShadow = "10px 20px 30px blue";
+//</div>
+function myFunction() {
+    document.getElementById("#spielfigur").style.boxShadow = "30px 30px 30px 30px black"; //verbunden mit Spielfeld.css 
+}
